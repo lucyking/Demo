9456420d6e62da69011c16f4964bede5f7f11a85
lucyking <joehisaishi1943@gmail.com>
2017-08-25 14:52:31 +0800

    +fx

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+int fx(int i);
 int main()
 {
 	int i = 0;
@@ -7,4 +8,11 @@ int main()
 		printf("%x %d %d\n",&i,i,i*i);
 		i=i+1;
 	}
+	fx(5);
+}
+
+int fx(int val)
+{
+	printf("%d %d\n",val,val&(val<<1));
+	return 0;
 }
